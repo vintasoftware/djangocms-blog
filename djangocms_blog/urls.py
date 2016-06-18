9361d581ff5f5d2712829093b20be36201b2b3aa
Guilherme Caminha <gpkc@cin.ufpe.br>
2016-06-18 17:08:28 -0300

    removed unused code

--- djangocms_blog/urls.py
+++ djangocms_blog/urls.py
@@ -4,22 +4,10 @@ from __future__ import absolute_import, print_function, unicode_literals
 from django.conf.urls import url
 
 from .feeds import FBInstantArticles, LatestEntriesFeed, TagFeed
-from .settings import get_setting
 from .views import (
     AuthorEntriesView, CategoryEntriesView, PostArchiveView, PostDetailView, PostListView,
     TaggedListView,
 )
-
-def get_urls():
-    urls = get_setting('PERMALINK_URLS')
-    details = []
-    for urlconf in urls.values():
-        details.append(
-            url(urlconf + r'$/', PostDetailView.as_view(), name='post-detail'),
-        )
-    return details
-
-detail_urls = get_urls()
 
 urlpatterns = [
     url(r'^$',
