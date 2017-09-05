bc143032917e81abe92af268bb56f0021f7f289b
Felipe Farias <felipelomba@gmail.com>
2017-09-05 20:16:34 -0300

    adding get_post_image templatetag

--- djangocms_blog/templatetags/vintasoftware_cms_tags.py
+++ djangocms_blog/templatetags/vintasoftware_cms_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from bs4 import BeautifulSoup
 
 register = template.Library()
 
@@ -6,3 +7,9 @@ register = template.Library()
 @register.filter
 def replace_character(character):
     return character.replace(' ','%20').replace('?','%3F')
+
+
+@register.filter
+def get_post_image(post):
+    get_post = BeautifulSoup(post, 'html.parser')
+    return get_post.img['src']
